194c0df1be7441f7905494932739a4ae38065041
ssibongee <ssibongee@gmail.com>
2021-02-20 02:37:49 +0900

    [#3] PostApiController 클래스 생성 및 게시물 저장 요청 처리 구현

---
+++ src/main/java/com/freelecspringboot2webservice/web/PostApiController.java
@@ -0,0 +1,21 @@
+package com.freelecspringboot2webservice.web;
+
+import com.freelecspringboot2webservice.service.post.PostService;
+import com.freelecspringboot2webservice.web.dto.PostSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class PostApiController {
+
+    private final PostService postService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostSaveRequestDto requestDto) {
+        return postService.save(requestDto);
+    }
+
+}

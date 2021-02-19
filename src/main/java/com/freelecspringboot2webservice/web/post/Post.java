73afffb8a1d3fba7fbe47f50483bb885fc094d46
ssibongee <ssibongee@gmail.com>
2021-02-20 02:36:32 +0900

    [#3] Post 엔티티 클래스 생성

---
+++ src/main/java/com/freelecspringboot2webservice/web/post/Post.java
@@ -0,0 +1,32 @@
+package com.freelecspringboot2webservice.web.post;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@RequiredArgsConstructor
+@Entity
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Post(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}

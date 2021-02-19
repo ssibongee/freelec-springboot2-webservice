5bff44035d5a4b92b3208aece96bcd462b14fda2
ssibongee <ssibongee@gmail.com>
2021-02-20 03:14:47 +0900

    [#3] JPA Auditing을 이용한 생성 및 수정시간 자동 생성 테스트

--- src/test/java/com/freelecspringboot2webservice/web/post/PostRepositoryTest.java
+++ src/test/java/com/freelecspringboot2webservice/web/post/PostRepositoryTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
@@ -43,6 +44,22 @@ class PostRepositoryTest {
         assertThat(post.getContent()).isEqualTo(content);
     }
 
+    @Test
+    @DisplayName("JPA Auditing을 사용하여 생성과 수정시간 자동 등록 테스트")
+    void baseTimeEntityTest() {
+        LocalDateTime now = LocalDateTime.of(2021, 2, 20, 0, 0, 0);
+        postRepository.save(Post.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
 
+        List<Post> posts = postRepository.findAll();
 
+        Post post = posts.get(0);
+
+        assertThat(post.getCreatedTime()).isAfter(now);
+        assertThat(post.getModifiedTime()).isAfter(now);
+
+    }
 }

bc0eb46918313f0c32cccd7dd0198c4faebeee2a
ssibongee <ssibongee@gmail.com>
2021-02-20 01:40:40 +0900

    [#1] GET("/hello") 요청에 대한 테스트 코드 작성

---
+++ src/test/java/com/freelecspringboot2webservice/web/HelloControllerTest.java
@@ -0,0 +1,30 @@
+package com.freelecspringboot2webservice.web;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(controllers = HelloController.class)
+class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    @DisplayName("/hello로 GET 요청을 보내면 'hello'라는 문자열이 반환된다.")
+    void responseHello() throws Exception {
+        String hello = "hello";
+
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+    }
+
+
+}

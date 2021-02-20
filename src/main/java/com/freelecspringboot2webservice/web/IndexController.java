2379b9d7313a3a2fa7dddaefea889d3a5153aaeb
ssibongee <ssibongee@gmail.com>
2021-02-21 00:50:46 +0900

    [#5] 전체 게시물을 조회하는 기능 구현

--- src/main/java/com/freelecspringboot2webservice/web/IndexController.java
+++ src/main/java/com/freelecspringboot2webservice/web/IndexController.java
@@ -1,13 +1,20 @@
 package com.freelecspringboot2webservice.web;
 
+import com.freelecspringboot2webservice.service.post.PostService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class IndexController {
 
+    private final PostService postService;
+
     @GetMapping
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("posts", postService.findAllDesc());
         return "index";
     }
 
